7592c38617e92bf7c3b0ea04cd8d59ca8384d78d
8r0ly <122978420+8r0ly@users.noreply.github.com>
2023-10-01 23:18:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let number = Math.floor(Math.random() * 10 + 1);
-let hintVal = document.getElementById("hint").value;
 document.getElementById("submitBtn").onclick = function() {
     let choice = document.getElementById("guessField").value;
     if(choice == number) {
